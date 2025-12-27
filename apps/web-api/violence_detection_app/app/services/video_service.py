13fecd63219d8fae982765e97e88fb37332b7389
Pamali-Ranasinghe <sapunikasr@gmail.com>
2025-12-27 14:27:00 +0530

    Create video service

---
+++ apps/web-api/violence_detection_app/app/services/video_service.py
@@ -0,0 +1,8 @@
+from violence_detection_app.src.data_processing.video_handler import VideoHandler
+
+class VideoService:
+
+    def __init__(self):
+        self.handler = VideoHandler()
+
+    # async def get_video_properties(self, file: )
