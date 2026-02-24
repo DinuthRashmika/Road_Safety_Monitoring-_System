5bc530d84b80fdb7bf8d0f8b5f10c6580f492d2d
Dinuth Rashmika <dinuthwelandagoda@gmail.com>
2026-02-24 13:52:56 +0530

    remove

--- apps/inference-service/backend/README.md
+++
@@ -1,4 +0,0 @@
-/*
-  venv\Scripts\activate
-  uvicorn app.main:app --reload --port 8000
-*/
--- apps/mobile/lib/models/token.dart
+++
@@ -1,7 +0,0 @@
-class TokenOut {
-  final String accessToken;
-  TokenOut({required this.accessToken});
-
-  factory TokenOut.fromJson(Map<String, dynamic> j) =>
-      TokenOut(accessToken: j['access_token'] as String);
-}
